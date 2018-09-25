912bc1f9de2612db38462e0728fad220a22c3a5f
Unknown <foxtrotalphalima@gmail.com>
2018-09-25 17:33:25 -0400

    Got rid of gin necessities.

--- vendor/github.com/sbrow/skirmish/doc.go
+++
@@ -1,8 +0,0 @@
-//go:generate mkmarkdown.sh
-
-// Package skirmish contains code for production of the
-// Dreamkeepers: Skirmish battle card game.
-//
-// More specifically, it provides an interface between the SQL database
-// that contains card data, Photoshop, and the user (via CLI).
-package skirmish
--- vendor/github.com/sbrow/skirmish/mkmarkdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pkg=github.com/sbrow/skirmish 
-
-godoc2md -template .doc.template $pkg > README.md
-branch=$(git rev-parse --abbrev-ref HEAD)
-sed -i -r "s/branch=master/branch=$branch/g" README.md

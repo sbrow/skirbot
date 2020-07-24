259ef40dd69a396e58dfb403c6a29bc91a90427e
Spencer Brower <foxtrotalphalima@gmail.com>
2020-07-24 11:20:23 -0400

    ci: Added build to terraform config.

--- skirmish-bot.tf
+++ skirmish-bot.tf
@@ -1,5 +1,6 @@
-provider "heroku" {
-  version = "~> 2.0"
+variable "heroku_api_key" {
+  description = "API token for authenticating heroku"
+  type        = string
 }
 
 variable "token" {
@@ -7,13 +8,30 @@ variable "token" {
   type        = string
 }
 
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "electricpandafishgames"
+
+    workspaces {
+      name = "skirmish-bot"
+    }
+  }
+}
+
+provider "heroku" {
+  version = "~> 2.0"
+  api_key = var.heroku_api_key
+}
+
+
 resource "heroku_app" "app" {
-  name   = "skirmish-bot"
-  region = "us"
+  name       = "skirmish-bot"
+  region     = "us"
   buildpacks = ["heroku/go"]
 
   sensitive_config_vars = {
-    TOKEN        = var.token
+    TOKEN = var.token
   }
 }
 
@@ -23,20 +41,18 @@ resource "heroku_addon" "database" {
   plan = "heroku-postgresql:hobby-dev"
 }
 
-/*
 resource "heroku_build" "example" {
-  app = heroku_app.example.name
+  app = heroku_app.app.name
 
   source = {
     url = "https://github.com/sbrow/skirbot/archive/master.tar.gz"
   }
 }
-*/
 
 resource "heroku_formation" "example" {
   app        = heroku_app.app.name
   type       = "worker"
   quantity   = 1
   size       = "Free"
-  #depends_on = [heroku_build.example]
+  depends_on = [heroku_build.example]
 }
